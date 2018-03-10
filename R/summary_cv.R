54a930721cd19714ba98d0173a150f518ac451c1
Nazli Ozum Kafaee <nazliozum@gmail.com>
2018-03-10 01:31:40 -0800

    Split Cross.R into separate function files

---
+++ R/summary_cv.R
@@ -0,0 +1,15 @@
+#' summary_cv: Summary statistics of cross-validation scores
+#'
+#' @param scores vector of cross-validation scores
+#'
+#' @return list of summary statistics consisting of mean, standard deviation, mode and median
+#'
+#' @export
+#'
+#' @examples
+#' cv_scores = c(0.97, 0.96, 0.98)
+#' summary_cv(scores = cv_scores)
+
+summary_cv <- function(scores){
+  # Get summary statistics here
+}
